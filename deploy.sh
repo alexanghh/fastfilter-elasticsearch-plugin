97e6ac97edc7435b4b3bb21ee77338ea485f6c7e
alexanghh <alexanghh@gmail.com>
2024-07-29 00:50:44 +0800

    added commands for port forwarding

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 kubectl delete elasticsearch elastic
 kubectl apply -f es.yaml
 PASSWORD=$(kubectl get secret elastic-es-elastic-user -o go-template='{{.data.elastic | base64decode}}') && echo $PASSWORD
+# kubectl port-forward service/elastic-kb-http 5601
+# kubectl port-forward service/elastic-es-http 9200
