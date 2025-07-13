1cf05f018b7c8a98ecc1348fc625f363735b9f62
alexanghh <alexanghh@gmail.com>
2025-07-13 17:59:55 +0800

    update scripts to install, build and deploy to k8s

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+# deploy es image to k8s
 kubectl delete elasticsearch elastic
 kubectl apply -f es.yaml
 PASSWORD=$(kubectl get secret elastic-es-elastic-user -o go-template='{{.data.elastic | base64decode}}') && echo $PASSWORD
